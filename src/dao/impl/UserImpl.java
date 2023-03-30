e3c37f7b1e11b63e3395bebfae0245dc4218067e
user <protanki002@gmail.com>
2023-03-30 08:21:30 +0600

    Fifth commit

--- src/dao/impl/UserImpl.java
+++ src/dao/impl/UserImpl.java
@@ -12,16 +12,16 @@ public class UserImpl implements UserDao {
 
     @Override
     public String addNewUser(User user) {
-        database.getUsers().add(user);
-        return "Successfully created";
+         database.getUsers().add(user);
+         return "Successfully created";
     }
 
     @Override
     public User userUpdateById(Long id, String nickName, String password, String photo) {
-        for (User u : database.getUsers()) {
-            if (id.equals(u.getId()) && password.equals(u.getPassword())) {
-                u.setNickName(nickName);
-                u.setPhoto(photo);
+         for (User u : database.getUsers()) {
+             if (id.equals(u.getId()) && password.equals(u.getPassword())) {
+                 u.setNickName(nickName);
+                 u.setPhoto(photo);
             }
         }
         return null;
@@ -29,9 +29,9 @@ public class UserImpl implements UserDao {
 
     @Override
     public User searchByName(String nickName) {
-        for (User u : database.getUsers()) {
-            if (u.getNickName().equals(nickName)) {
-                return u;
+         for (User u : database.getUsers()) {
+             if (u.getNickName().equals(nickName)) {
+                 return u;
             }
         }
         return null;
@@ -40,25 +40,25 @@ public class UserImpl implements UserDao {
     @Override
     public User sortByAge() {
         // stream//сортировка по возрасту (по возрастанию и убыванию)
-        System.out.println(database.getUsers().stream().sorted(Comparator.comparingInt(User::getAge)).findAny().orElse(null));
-        return database.getUsers()
-                .stream()
-                .sorted(Comparator.comparingInt(User::getAge))
-                .findAny()
-                .orElse(null);
+         System.out.println(database.getUsers().stream().sorted(Comparator.comparingInt(User::getAge)).findAny().orElse(null));
+         return database.getUsers()
+                 .stream()
+                 .sorted(Comparator.comparingInt(User::getAge))
+                 .findAny()
+                 .orElse(null);
     }
 
 
     @Override
     public User filter() {
-        System.out.println("1 - person < 20  |  2 - person > 20");
-        int inp = new Scanner(System.in).nextInt();
-        if (inp == 1) {
+         System.out.println("1 - person < 20  |  2 - person > 20");
+         int inp = new Scanner(System.in).nextInt();
+         if (inp == 1) {
             database.getUsers().stream().filter(user -> user.getAge() < 20).forEach(System.out::println);
-        }
-        if (inp == 2){
+         }
+         if (inp == 2){
             database.getUsers().stream().filter(user -> user.getAge() > 20).forEach(System.out::println);
-        }
+         }
         return null;
     }
 }
--- src/db/Database.java
+++ src/db/Database.java
@@ -8,26 +8,24 @@ import java.util.List;
 public class Database {
 List<User> users = new ArrayList<>();
 public Database (){
-
 }
     public Database(List<User> users) {
-        this.users = users;
-    }
+        this.users = users;}
 
 
     public List<User> getUsers() {
         return users;
-    }
+}
 
     public void setUsers(List<User> users) {
         this.users = users;
-    }
+}
 
     @Override
     public String toString() {
         return "Database{" +
                 "users=" + users +
                 '}';
-    }
-    // stream//сортировка по возрасту (по возрастанию и убыванию)
+}
+    //stream//сортировка по возрасту (по возрастанию и убыванию)
 }
